7f24fcd62dae68254410922d98a573a954a624ad
Kilyan Slowinski <kilyan.slowinski@etudiant.univ-reims.fr>
2024-04-06 18:54:41 +0200

    maj .h

--- linear_system.c
+++ linear_system.c
@@ -1,1 +1,7 @@
 #include "linear_system.h"
+
+void read_system(linear_system* system, char* path);
+void save_system(linear_system* system, char* path);
+linear_system* generate_system(int len);
+void find_pivot(/*truc*/);
+void swap_rows(int* row1, int* row2);
--- linear_system.h
+++ linear_system.h
@@ -9,9 +9,9 @@ typedef struct linear_system_struct
     int len;
 } linear_system ;
 
-void read_system(linear_system system, char* path);
-void save_system(linear_system system, char* path);
-int** generate_system(int len);
+void read_system(linear_system* system, char* path);
+void save_system(linear_system* system, char* path);
+linear_system* generate_system(int len);
 void find_pivot(/*truc*/);
 void swap_rows(int* row1, int* row2);
 
