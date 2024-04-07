db84fd8c6bb35b812d277fcdd7101caec593805a
Kilyan Slowinski <kilyan.slowinski@etudiant.univ-reims.fr>
2024-04-07 02:26:09 +0200

    ok pour le solver | todo partie save solution

--- iterative_solver.c
+++ iterative_solver.c
@@ -11,14 +11,17 @@
 // 
 //      !! -> garder en mémoire les lignes qu'on swap, (tableau d'entier par lequel on passe pour obtenir les valeurs ?)
 // WIP
-void solve_system(linear_system* system){
+// system : le système à solve
+// path : le path vers le fichier de sortie, qui contiendra la solution
+// verbose : sortie uniquement dans le fichier de sortie si verbose = 0, infos basiques dans la sortie standard si verbose = 1, infos avancée si verbose = 2 (à implémenter)
+void solve_system(linear_system* system, char* path, int verbose){
     int pivot_line = 0;
     int* lines_link;
     lines_link = (int*)malloc(system->len * sizeof(int));
     
     for (int i = 0; i < system->len; i++)
     {
-        
+        lines_link[i] = i;
     }
     
 
@@ -29,18 +32,40 @@ void solve_system(linear_system* system){
 
         //Echange des lignes si besoin
         if(pivot_line != pivot_row){
-            swap_lines(system,  lines_link, pivot_line, pivot_row);
+            swap_lines(system, lines_link, pivot_line, pivot_row);
         }
         
-
+        for(int line_to_change = pivot_row+1; line_to_change < system->len; line_to_change++){
+            apply_pivot_to_line(system, lines_link, line_to_change, pivot_row);
+        }
 
     }
+
+    if(verbose > 0){
+        printf("Sauvegarde de la solution : %.64s\n");
+    }
+    save_solution(system, lines_link, path);
+
+    free(lines_link);
 }
 
 // Renvoies la ligne du pivot
 // row -> la colonne du pivot
-// TODO
-int find_pivot_for_row(linear_system* sys, int* lines_link, int row);
+// La ligne pivot est celle détenant la plus grande valeur sur la colonne recherchée
+int find_pivot_for_row(linear_system* sys, int* lines_link, int row){
+    int pivot_line = row;
+    int pivot_value = sys->equation[row][row];
+    int cur_value = 0;
+    for (int cur_line = row+1; cur_line < sys->len; cur_line++)
+    {
+        cur_value = sys->equation[cur_line][row];
+        if(cur_value > pivot_value){
+            pivot_line = cur_line;
+            pivot_value = cur_value;
+        }
+    }
+    return pivot_line;
+}
 // On effectue ce changement uniquement dans le tableau d'entier pour garder sa forme originale dans le système linéaire 
 // On va donc en permanence accéder aux données via ce tableau 
 // Ça va permettre de simplifier la sauvegarde des données de sortie 
@@ -56,10 +81,25 @@ void swap_lines(linear_system* sys ,int* lines_link, int line1, int line2){
     lines_link[line1] = lines_link[line2];
     lines_link[line2] = tmp;
 }
-
+// applique le pivot sur la ligne passé en paramètre en effectuant la multiplication nécessaire
+// comme on a swap les lignes pivot row est normalement = pivot line 
+// peut-être besoin de passer par lines link dépendant comment on gère ? 
+void apply_pivot_to_line(linear_system* sys, int* lines_link, int target_line, int pivot_row){
+    
+    // Calcul du coefficient
+    float multiplier = 1.0f; 
+    multiplier = sys->equation[target_line][pivot_row] / sys->equation[pivot_row][pivot_row];
+    
+    // Application du pivot sur la ligne
+    for (int i = 0; i < sys->len; i++)
+    {
+        sys->equation[target_line][i] = sys->equation[target_line][i] - multiplier * sys->equation[pivot_row][i];
+    }
+    
+}
 // la fonction est bien différente de celle utilisée par le générateur de système 
 // puisqu'elle doit prendre en compte que des lignes ont été swaps 
 // TODO
-void save_solution(linear_system* sys, int* lines, char* path){
+void save_solution(linear_system* sys, int* lines_link, char* path){
 
 }
--- iterative_solver.h
+++ iterative_solver.h
@@ -3,6 +3,8 @@
 #include "linear_system.h"
 int find_pivot_for_row(linear_system* sys, int* lines_link, int row);
 void swap_lines(linear_system* sys, int line1, int line2);
+// applique le pivot sur la ligne passé en paramètre en effectuant la multiplication nécessaire
+void apply_pivot_to_line(linear_system* sys, int* lines_link, int target_line, int pivot_row);
 void solve_system(linear_system* system);
-void save_solution(linear_system* sys, int* lines, char* path);
+void save_solution(linear_system* sys, int* lines_link, char* path);
 #endif
