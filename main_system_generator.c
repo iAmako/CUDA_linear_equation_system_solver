d09b1c3a50e40022067c4b5cbba5c4a15838ada7
Kilyan Slowinski <kilyan.slowinski@etudiant.univ-reims.fr>
2024-04-08 09:23:02 +0200

    gitignore data  files | generator args fixed

--- main_system_generator.c
+++ main_system_generator.c
@@ -16,13 +16,13 @@ int main(int argc, char const *argv[])
 
     
     int count = 1;
-    if(argc == 3 ){
-        count = atoi(argv[3]);
+    if(argc == 2 ){
+        count = atoi(argv[2]);
     }
 
     int len = 30;
-    if(argc > 1 ){
-        len = atoi(argv[2]);
+    if(argc > 0 ){
+        len = atoi(argv[1]);
     }
 
     linear_system* sys;
