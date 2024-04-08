d628bbdcc866966fe1b2bb5f4ef443d08884a269
Kilyan Slowinski <kilyan.slowinski@etudiant.univ-reims.fr>
2024-04-08 10:00:42 +0200

    indications lancement main

--- main_system_generator.c
+++ main_system_generator.c
@@ -2,46 +2,48 @@
 #include "iterative_solver.h"
 #include <time.h>
 
-//main generateur de systemes
-//enregsitré sous ./systems/sys_horodatage.txt
+// main generateur de systemes
+// enregsitré sous ./systems/sys_horodatage.txt
 int main(int argc, char const *argv[])
 {
 
-    printf("Utilisation : system_generator LEN NB\nAvec :\n\tLEN : La taille des systèmes (Défaut : 30) \n\tNB : Le nombre de systèmes à générer (Défaut : 1)\n");
-
+    if (argc > 2)
+    {
+        printf("Utilisation : system_generator LEN NB\nAvec :\n\tLEN : La taille des systèmes (Défaut : 30) \n\tNB : Le nombre de systèmes à générer (Défaut : 1)\n");
+    }
     char filename[64];
     time_t cur_time;
 
     struct tm *time_str;
 
-    
     int count = 1;
-    if(argc == 2 ){
+    if (argc == 2)
+    {
         count = atoi(argv[2]);
     }
 
     int len = 30;
-    if(argc > 0 ){
+    if (argc > 0)
+    {
         len = atoi(argv[1]);
     }
 
-    linear_system* sys;
+    linear_system *sys;
 
     for (int i = 0; i < count; i++)
     {
         cur_time = time(NULL);
         time_str = gmtime(&cur_time);
         snprintf(filename, sizeof(filename), "./systems/sys_%d_%d-%d-%d_%d-%d-%d.txt", len, time_str->tm_year,
-                                                                                            time_str->tm_mon,
-                                                                                            time_str->tm_mday,
-                                                                                            time_str->tm_hour,
-                                                                                            time_str->tm_min,
-                                                                                            time_str->tm_sec);
+                 time_str->tm_mon,
+                 time_str->tm_mday,
+                 time_str->tm_hour,
+                 time_str->tm_min,
+                 time_str->tm_sec);
         sys = generate_system(len);
-        save_system(sys,filename);
+        save_system(sys, filename);
         free_system(sys);
     }
-    
 
     return 0;
 }
